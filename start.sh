02a793a4369df77131dd554bc91406d4595a029d
PERUSIOFFICE12 <149090852+PERUSIOFFICE12@users.noreply.github.com>
2025-08-12 12:45:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ffmpeg -re -i "https://archive.org/download/video_20250812_0903/video.mp4" \
+ffmpeg -re -stream_loop -1 -i "https://archive.org/download/video_20250812_0903/video.mp4" \
   -vf scale=1280:720 \
   -c:v libx264 -preset veryfast -maxrate 1500k -bufsize 3000k \
   -c:a aac -b:a 96k -ar 44100 \
