e887decf3be62e847f7cbdeccc535c426c8c33f0
PERUSIOFFICE12 <149090852+PERUSIOFFICE12@users.noreply.github.com>
2025-08-12 12:10:36 +0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true; do
+  ffmpeg -re -stream_loop -1 -i "https://archive.org/download/video_20250812_0903/video.mp4" \
+    -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv "rtmp://a.rtmp.youtube.com/live2/$YT_KEY"
+done
