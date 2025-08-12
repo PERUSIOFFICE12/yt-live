82e16f252e46072d81ad36eeaade9d5cfefaefc5
PERUSIOFFICE12 <149090852+PERUSIOFFICE12@users.noreply.github.com>
2025-08-12 12:46:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+
 ffmpeg -re -stream_loop -1 -i "https://archive.org/download/video_20250812_0903/video.mp4" \
-  -vf scale=1280:720 \
-  -c:v libx264 -preset veryfast -maxrate 1500k -bufsize 3000k \
-  -c:a aac -b:a 96k -ar 44100 \
-  -f flv "rtmp://a.rtmp.youtube.com/live2/$YT_KEY"
+-vf scale=1280:720 \
+-r 30 \
+-c:v libx264 -preset veryfast -b:v 1500k -maxrate 1500k -bufsize 3000k \
+-c:a aac -b:a 128k -ar 44100 \
+-f flv "rtmp://a.rtmp.youtube.com/live2/$YT_KEY"
