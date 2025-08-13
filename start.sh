c2a4ac7c273db009c75f985e1118f46f8524d66a
PERUSIOFFICE12 <149090852+PERUSIOFFICE12@users.noreply.github.com>
2025-08-13 19:53:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # Descarcă video-ul o singură dată
 wget -O video.mp4 "https://archive.org/download/video_20250812_0903/video.mp4"
 
-# Rulează live pe infinite loop
+# Rulează live pe infinite loop la 1080p
 ffmpeg -re -stream_loop -1 -i video.mp4 \
   -vf scale=1920:1080 \
-  -c:v libx264 -preset veryfast -maxrate 1500k -bufsize 3000k \
-  -c:a aac -b:a 96k -ar 44100 \
+  -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
+  -c:a aac -b:a 128k -ar 44100 \
   -f flv "rtmp://a.rtmp.youtube.com/live2/$YT_KEY"
