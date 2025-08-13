225c2887ac4b6339b4bb9453c2a642453c1f95fc
PERUSIOFFICE12 <149090852+PERUSIOFFICE12@users.noreply.github.com>
2025-08-13 23:14:32 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ fi
 ffmpeg -nostdin -re -stream_loop -1 -i video.mp4 \
 -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p \
 -c:a aac -b:a 128k -ar 44100 \
--f flv rtmp://a.rtmp.youtube.com/live2/YOUR_STREAM_KEY
+-f flv rtmp://a.rtmp.youtube.com/live2/b1u3-2mbf-3532-8u2u-5yfe
