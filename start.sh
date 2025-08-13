48413a92237fcefb75c6ae8c84dfa8c1bbe967bd
PERUSIOFFICE12 <149090852+PERUSIOFFICE12@users.noreply.github.com>
2025-08-13 21:40:30 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Descarcă video-ul o singură dată
-wget -O video.mp4 "https://archive.org/download/video_20250812_0903/video.mp4"
+# Dacă fișierul nu există, îl descarcă
+if [ ! -f video.mp4 ]; then
+    wget -O video.mp4 "https://archive.org/download/video_20250812_0903/video.mp4"
+fi
 
-# Rulează live pe infinite loop la 1080p
+# Rulează live pe loop infinit
 ffmpeg -re -stream_loop -1 -i video.mp4 \
   -vf scale=1920:1080 \
   -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
